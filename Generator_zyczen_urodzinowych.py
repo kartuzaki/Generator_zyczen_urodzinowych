aea1c5347360e534b0c0162e1231a08f5c8b212c
kartuzaki <patryk.w.zielinski@gmail.com>
2024-02-16 19:10:11 +0100

    Dodałem opcję wybierania płci

--- Generator_zyczen_urodzinowych.py
+++ Generator_zyczen_urodzinowych.py
@@ -1,11 +1,14 @@
 print("Witaj w Życzeniomacie!")
-print("Ten program wygeneruje dla Ciebie krótkie życzenia urodzinowe.\nW kolejnych krokach postępuj zgodnie z poleceniami:\n")
+print("Ten program wygeneruje dla Ciebie krótkie życzenia urodzinowe.\nW kolejnych krokach postępuj zgodnie z "
+      "poleceniami:\n")
 print("Podaj dzisiejszą datę:\nDzień/Miesiąc...:") 
 data_dzien = input()
 print("...oraz rok")
 data_rok = int(input())
 print("Podaj swoje imię:") 
 imie_nadawcy = str(input())
+print("Podaj swoją płeć (K/M):")
+sex = input()
 print("Podaj imię jubilat/ka:")
 imie_odbiorcy = input()
 print("Podaj rok urodzenia jubilata/ki:")
@@ -14,6 +17,12 @@ print("Dokończ zdanie: życzę Ci...")
 zyczenia = input()
 wiek = (data_rok - data_urodzenia)
 print("\n __________________________________________________________________________________________<3\n|")
-print(f"|Witaj {imie_odbiorcy}!\n|\n|Z okazji Twoich {wiek} urodzin, życzę Ci {zyczenia}!\n|\n|~{imie_nadawcy}!")
+print(f"|Witaj {imie_odbiorcy}!\n|\n|Z okazji Twoich {wiek} urodzin, życzę Ci {zyczenia}!\n|")
+if sex == "K":
+      print(f"|~Twoja serdeczna przyjaciółka {imie_nadawcy}!")
+elif sex == "M":
+      print(f"|~Twój serdeczny przyjaciel {imie_nadawcy}!")
+else:
+      print(f"|~{imie_nadawcy}!")
 print("|__________________________________________________________________________________________<3\n")
 #ver1.2 - wersja rozwojowa
